cb829ac7abfcbb0e4f1dfc1144e3ba29bedefef3
Sourav halder <souravh093@gmail.com>
2023-06-07 19:18:26 +0600

    initial server setup done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Music Maestro server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
